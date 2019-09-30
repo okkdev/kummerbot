8a4cd35837e339a793f39be4728fbca5f94def5d
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-09-30 17:00:59 +0200

    custom titles

--- lib/kummerbot/command/util.ex
+++ lib/kummerbot/command/util.ex
@@ -16,11 +16,21 @@ defmodule Kummerbot.Command.Util do
   def mail(msg) do
     Api.create_message(msg.channel_id, "Thank you :slight_smile:")
 
+    [name, content] =
+      if String.starts_with?(msg.content, "!") do
+        msg.content
+        |> String.trim()
+        |> String.trim_leading("!")
+        |> String.split(" ", parts: 2)
+      else
+        ["Anonymous", msg.content]
+      end
+
     embedmsg =
       %Nostrum.Struct.Embed{}
-      |> put_title("Anonymous")
-      |> put_description(msg.content)
-      |> put_color(Enum.random(1..16777215))
+      |> put_title(name)
+      |> put_description(content)
+      |> put_color(Enum.random(1..16_777_215))
       |> put_image(get_image_url(msg))
 
     Api.create_message(Application.get_env(:kummerbot, :kummerchannel), embed: embedmsg)
