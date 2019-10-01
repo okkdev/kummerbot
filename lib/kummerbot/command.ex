45a3fbcf03ace87c67845ec57c0e7fd240fc82b2
hactar <hactar@cyberkraft.org>
2019-10-01 13:41:33 +0200

    Make use of fun guards in command module

--- lib/kummerbot/command.ex
+++ lib/kummerbot/command.ex
@@ -3,20 +3,11 @@ defmodule Kummerbot.Command do
 
   @bot_id Application.get_env(:kummerbot, :bot_id)
 
-  defp actionable_command?(msg) do
-    msg.author.id != @bot_id
-  end
+  defguardp is_actionable_command(msg) when msg.author.id != @bot_id
+  defguardp is_direct_message(msg) when msg.guild_id == nil
 
-  defp direct_message?(msg) do
-    msg.guild_id == nil
-  end
-
-  def handle(msg) do
-    if actionable_command?(msg) do
-      if direct_message?(msg) do
-        Util.mail(msg)
-      end
-    end
+  def handle(msg) when is_actionable_command(msg) and is_direct_message(msg) do
+    Util.mail(msg)
   end
 
   def execute(_any, _msg) do
