f7e5bf65b456f56ef30bdb3d17656fdeca7feeab
okkdev <stehlik.jen@gmail.com>
2020-11-12 10:32:32 +0100

    ✅ update test

--- test/kummerbot_test.exs
+++ test/kummerbot_test.exs
@@ -1,8 +1,24 @@
 defmodule KummerbotTest do
   use ExUnit.Case
-  doctest Kummerbot
+  alias Kummerbot.Command.Mailer
 
-  test "greets the world" do
-    assert Kummerbot.hello() == :world
+  @test_message %Nostrum.Struct.Message{
+    attachments: [],
+    author: %Nostrum.Struct.User{
+      bot: nil,
+      discriminator: "2094",
+      id: 116146991540600840,
+      username: "okk"
+    },
+    channel_id: 583947157338456064,
+    content: "Test",
+    edited_timestamp: nil,
+    embeds: [],
+    guild_id: nil
+  }
+
+  test "Send test message" do
+    {result, _} = Mailer.mail(@test_message)
+    assert :ok == result
   end
 end
